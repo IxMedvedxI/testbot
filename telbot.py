ca1f6c0fe8ffabadfc1cc7c9009c4548f44d7457
Максим <90824803+IxMedvedxI@users.noreply.github.com>
2022-08-09 02:36:14 +0300

    fun

--- telbot.py
+++ telbot.py
@@ -10,12 +10,11 @@ def fun(st):
   y = "яЯ"
   st.split()
   sting =''
-  if y[0] in st or y[1] in st:
-    for i in range(len(st)):
-      if st[i] == y[0] or st[i] == y[1]:
-        sting += 'ты'
-      else:
-        sting += st[i] 
+   for i in range(len(st)):
+    if st[i] == y[0] or st[i] == y[1]:
+       sting += 'ты'
+    else:
+       sting += st[i] 
   return sting
 
 token ='5413641390:AAEcZAElsbwbWXkO6e8Fhwj6RFAHNB2892c'
